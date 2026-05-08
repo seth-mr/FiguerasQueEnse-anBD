fa546cf11fe1b2334987b4195d07529285557c44
Seth Marquez <cuentadesoportea@gmail.com>
2026-05-07 20:29:08 -0600

    x

--- MicroservicioFiguras/Models/FigurasqeContext.cs
+++ MicroservicioFiguras/Models/FigurasqeContext.cs
@@ -28,8 +28,18 @@ public partial class FigurasqeContext : DbContext
     public virtual DbSet<Tutor> Tutors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=figurasqe;Username=postgres;Password=1234");
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            return;
+        }
+
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__PostgresConnection");
+        if (!string.IsNullOrWhiteSpace(connectionString))
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
